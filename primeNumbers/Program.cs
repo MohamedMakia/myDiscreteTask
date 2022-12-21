eeb863222ec68d51a1037bfa537557752165718d
MohamedMakia <77468657+MohamedMakia@users.noreply.github.com>
2022-12-21 07:57:07 +0200

    Add files via upload

---
+++ primeNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace MyProgram
+{
+    class Myclass
+    {
+
+        static void Main()
+        {
+            
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i =n1; i<=n2; i++)
+            {
+                int e = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        e++;
+                        break;
+                    }
+                    
+                  
+                                    
+                    
+                }
+                if(e == 0 && i !=1)
+                {
+                    Console.Write(i + " ");
+                }
+
+
+            }
+           
+
+
+                
+
+        }
+    }
+}
+
+
+
+
+    
+
+
+
+
+
+
